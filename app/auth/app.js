5b11f7ee5f4cd9a7bf0207b3fff88b1f1606dc39
Lloyd <leblanc71u@etu.univ-lorraine.fr>
2023-03-27 15:28:53 +0200

    bug fix
    bug fix

---
+++ app/auth/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const authRouter = require("./routes/auth");
+
+let app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/auth", authRouter);
+
+app.use("*", (req, res) => {
+  res
+    .status(404)
+    .json({ type: "error", error: 404, message: "page introuvable" });
+});
+
+module.exports = app;
